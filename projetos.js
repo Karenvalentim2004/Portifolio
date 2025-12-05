e1229671dd30de366d74c8acd58553560c28a5ef
Karen Valentim <karenvalentim54@gmail.com>
2025-12-04 21:45:46 -0300

    Atualização página projetos

--- projetos.js
+++ projetos.js
@@ -1,15 +1,42 @@
 
-    const cards = document.querySelectorAll(".open-modal");
-    const modal = document.getElementById("modal");
+// <!-- MODAL -->
 
-    cards.forEach(card => {
-        card.addEventListener("click", () => {
-            document.getElementById("modal-img").src = card.dataset.img;
-            document.getElementById("modal-title").innerText = card.dataset.title;
-            document.getElementById("modal-date").innerText = card.dataset.date;
-            document.getElementById("modal-desc").innerText = card.dataset.desc;
+const modal = document.getElementById("modal-projeto");
+const closeBtn = document.querySelector(".close");
 
-            modal.classList.add("show");
-        });
+document.querySelectorAll(".card").forEach(card => {
+    card.querySelector(".open-modal").addEventListener("click", () => {
+
+        document.getElementById("modal-title").textContent =
+            card.dataset.title;
+
+        document.getElementById("modal-img").src =
+            card.dataset.img;
+
+        document.getElementById("modal-date").textContent =
+            card.dataset.date;
+
+        document.getElementById("modal-desc").textContent =
+            card.dataset.desc;
+
+        document.getElementById("modal-preview").href =
+            card.dataset.preview;
+
+        modal.classList.add("show");
     });
+});
 
+closeBtn.addEventListener("click", () => {
+    modal.classList.remove("show");
+});
+
+
+        // VLibras
+
+         <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+    <script>
+        new window.VLibras.Widget('https://vlibras.gov.br/app');
+    </script>
+
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>AOS.init();</script>
